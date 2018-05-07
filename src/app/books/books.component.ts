77110d405ae64cc2a278683538e1e36ce4281c6b
Paul Fricker <paul_fricker@hms.harvard.edu>
2018-05-07 16:58:13 -0400

    added a little more logic to the components

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 
-export class BooksComponent { }
+export class BooksComponent {
+  books: any [] = [
+    {
+      "bookID": 1,
+      "title": "Harry Potter and the Prisoner of Azkaban",
+      "author": "J.K. Rowling",
+      "format": "Hardcover",
+      "available": "NO"
+    },
+    {
+      "bookID": 2,
+      "title": "Dresden Files: Cold Days",
+      "author": "Jim Butcher",
+      "format": "Paperback",
+      "available": "YES"
+    },
+    {
+      "bookID": 3,
+      "title": "Old Man's War",
+      "author": "John Scalzi",
+      "format": "Paperback",
+      "available": "YES"
+    },
+  ]
+}
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 
-export class MoviesComponent { }
+export class MoviesComponent {
+  movies: any[] = [
+    {
+      "title": "John Wick",
+      "year": 2014,
+      "format": "Blu-Ray",
+      "Available": true
+    },
+    {
+      "title": "The Fellowship of the Ring",
+      "year": 2001,
+      "format": "DVD",
+      "Available": false
+    },
+    {
+      "title": "Thor: Ragnarok",
+      "year": 2017,
+      "format": "Blu-Ray",
+      "Available": true
+    }
+  ]
+}
