cf2814dd69886e1b0fdd240e474f19e24fb781f3
Paul Fricker <paul_fricker@hms.harvard.edu>
2018-05-09 21:18:54 -0400

    fixed stupid mistake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { BookService } from "./books/books.service";
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [BookService]
 })
 export class AppComponent {
   title = 'Office Lending Library';
